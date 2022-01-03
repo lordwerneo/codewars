a51b7fda66a82ec414526f712947b90bee925b47
jlTsZGpw <lordwerneo@gmail.com>
2022-01-03 03:41:32 +0200

    Add 4 5kyu tasks

---
+++ 5kyu/Human Readable Time.py
@@ -0,0 +1,16 @@
+"""
+https://www.codewars.com/kata/52685f7382004e774f0001f7
+
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+"""
+
+
+def make_readable(seconds):
+    return f'{int(seconds//3600):02d}:{int(seconds%3600//60):02d}:{int(seconds%3600%60):02d}'
